1c44d3c43c80a09da1e3ca401b5ccc80023f39d8
Jason Barr <jason@jasonsbarr.com>
2019-05-18 13:55:34 -0500

    Style character info list item, make default box-sizing: border-box

--- starwars/src/components/CharacterList/Character.js
+++ starwars/src/components/CharacterList/Character.js
@@ -3,38 +3,42 @@ import "../StarWars.css";
 
 const Character = ({ character }) => {
   return (
-    <li key={character.url} className="character-item">
+    <li key={character.url} className="character">
       <ul className="character-info">
         <li className="character-info-item">
           <h2 className="character-info-item-heading">{character.name}</h2>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Born: </span>
-          {character.birth_year}
+          <span className="character-item-content">{character.birth_year}</span>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Gender: </span>
-          {character.gender}
+          <span className="character-item-content">{character.gender}</span>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Height: </span>
-          {`${character.height} cm`}
+          <span className="character-item-content">
+            {`${character.height} cm`}
+          </span>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Mass: </span>
-          {`${character.mass} kg`}
+          <span className="character-item-content">
+            {`${character.mass} kg`}
+          </span>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Hair: </span>
-          {character.hair_color}
+          <span className="character-item-content">{character.hair_color}</span>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Skin: </span>
-          {character.skin_color}
+          <span className="character-item-content">{character.skin_color}</span>
         </li>
         <li className="character-info-item">
           <span className="character-item-label">Eyes: </span>
-          {character.eye_color}
+          <span className="character-item-content">{character.eye_color}</span>
         </li>
       </ul>
     </li>
--- starwars/src/components/CharacterList/CharacterList.js
+++ starwars/src/components/CharacterList/CharacterList.js
@@ -5,7 +5,7 @@ import "../StarWars.css";
 const CharacterList = ({ characters }) => {
   return (
     <div id="star-wars-characters" className="characters">
-      <ul className="character-list">
+      <ul className="characters-list">
         {characters.map(character => (
           <Character character={character} />
         ))}
