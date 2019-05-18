2d66210419745a65e903b99a46c5900015954502
Jason Barr <jason@jasonsbarr.com>
2019-05-18 14:56:47 -0500

    Add previous and next page buttons to characters list

--- starwars/src/components/CharacterList/CharacterList.js
+++ starwars/src/components/CharacterList/CharacterList.js
@@ -2,7 +2,13 @@ import React from "react";
 import Character from "./Character";
 import "../StarWars.css";
 
-const CharacterList = ({ characters }) => {
+const CharacterList = ({ characters, ...props }) => {
+  const showPageButton = page => {
+    return props[page.toLowerCase()] ? (
+      <button className={`btn btn-page ${page}`}>{page}</button>
+    ) : null;
+  };
+
   return (
     <div id="star-wars-characters" className="characters">
       <ul className="characters-list">
@@ -10,6 +16,10 @@ const CharacterList = ({ characters }) => {
           <Character character={character} />
         ))}
       </ul>
+      <div className="characters-controls">
+        {showPageButton("Previous")}
+        {showPageButton("Next")}
+      </div>
     </div>
   );
 };
