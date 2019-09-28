a83284231074bee7fb11e9b992405d39986f1d86
Jason Barr <jason@jasonsbarr.com>
2019-09-28 14:04:17 -0500

    Style character listings

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,20 +1,58 @@
 import React from "react";
+import styled from "styled-components";
+
+const CharactersContainer = styled.div`
+  background-color: rgba(61, 181, 229, 0.35);
+  border-radius: 5px;
+  margin: 0 auto 1rem;
+  max-width: 500px;
+  min-height: 100vh;
+  padding: 1.5rem;
+`;
+
+const CharacterName = styled.h2`
+  color: #2b2d2f;
+  font-family: "Star Jedi", sans-serif;
+  font-size: 2rem;
+  text-align: center;
+`;
+
+const Label = styled.span`
+  font-family: "Courier New", Courier, monospace;
+  font-size: 1.3rem;
+  font-weight: bold;
+`;
+
+const Attribute = styled.p`
+  margin: 0 auto 0.5rem;
+  width: 50%;
+`;
 
 const CharacterList = ({ characters }) => {
   return (
-    <div className="characterContainer">
+    <CharactersContainer>
       {characters
         ? characters.map(character => (
-            <div key={character.url} className="singleCharacter">
-              <h3 className="characterName">{character.name}</h3>
-              <p>{character.height}</p>
-              <p>{character.mass}</p>
-              <p>{character.hair_color}</p>
-              <p>{character.eye_color}</p>
+            <div key={character.url}>
+              <CharacterName className="characterName">
+                {character.name}
+              </CharacterName>
+              <Attribute>
+                <Label>Height:</Label> {character.height} cm
+              </Attribute>
+              <Attribute>
+                <Label>Mass:</Label> {character.mass} kg
+              </Attribute>
+              <Attribute>
+                <Label>Hair:</Label> {character.hair_color}
+              </Attribute>
+              <Attribute>
+                <Label>Eyes:</Label> {character.eye_color}
+              </Attribute>
             </div>
           ))
         : "Loading..."}
-    </div>
+    </CharactersContainer>
   );
 };
 
