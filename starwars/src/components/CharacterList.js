ab0131313e8696c0081a3758fe7f9572dcf945d2
Jason Barr <jason@jasonsbarr.com>
2019-09-28 13:12:26 -0500

    Map characters array to JSX elements

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const CharacterList = props => {
-  return <p>Characters go here</p>;
+const CharacterList = ({ characters }) => {
+  return (
+    <div className="characterContainer">
+      {characters
+        ? characters.map(character => (
+            <div key={character.url} className="singleCharacter">
+              <h3 className="characterName">{character.name}</h3>
+              <p>{character.height}</p>
+              <p>{character.mass}</p>
+              <p>{character.hair_color}</p>
+              <p>{character.eye_color}</p>
+            </div>
+          ))
+        : "Loading..."}
+    </div>
+  );
 };
 
 export default CharacterList;
